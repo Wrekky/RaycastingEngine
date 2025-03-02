ec578a67aee9bc2127017690b62235c7861de88d
Andrew Friesen <andrewf5000@gmail.com>
2025-03-02 17:25:15 -0500

    added player collision and a line showing player direction

--- javascriptVersion/raycasting/raycast.js
+++ javascriptVersion/raycasting/raycast.js
@@ -22,6 +22,14 @@ class Map {
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         ];
     }
+    hasWallAt(x,y) {
+        if(x < 0 || x > WINDOW_WIDTH || y < 0 || y > WINDOW_HEIGHT) {
+            return true;
+        }
+        var mapGridIndexX = Math.floor(x / TILE_SIZE);
+        var mapGridIndexY = Math.floor(y / TILE_SIZE);
+        return this.grid[mapGridIndexY][mapGridIndexX] != 0;
+    }
     render() {
         for (var i = 0; i < MAP_NUM_ROWS; i++) {
             for (var j = 0; j < MAP_NUM_COLS; j++) {
@@ -35,7 +43,7 @@ class Map {
         }
     }
 }
-
+var grid = new Map();
 class Player {
     constructor() {
         this.x = WINDOW_WIDTH / 2;
@@ -50,17 +58,30 @@ class Player {
     render() {
         fill("red");
         circle(this.x, this.y, this.radius);
+        stroke('magenta');
+        strokeWeight(1);
+        line(this.x, this.y, this.x + Math.cos(this.rotationAngle) * 20,this.y + Math.sin(this.rotationAngle) * 20);
     }
     update() {
         this.rotationAngle += this.turnDirection * this.rotationSpeed;
+        //y wall collision
+        var xStep = Math.cos(this.rotationAngle) * (this.moveSpeed * this.walkDirection);
+        var yStep = Math.sin(this.rotationAngle) * (this.moveSpeed * this.walkDirection);
         if (this.walkDirection != 0) {
-            this.y += Math.sin(this.rotationAngle) * (this.moveSpeed * this.walkDirection);
-            this.x += Math.cos(this.rotationAngle) * (this.moveSpeed * this.walkDirection);
+            this.y += yStep;
+            if (grid.hasWallAt(this.x, this.y) == 1) {
+                this.y -= yStep;
+            }
+            //check collision -> convert position to grid.
+            this.x += xStep;
+            if (grid.hasWallAt(this.x, this.y) == 1) {
+                this.x -= xStep;
+            }
         }
 
     }
 }
-var grid = new Map();
+
 var player = new Player();
 
 function keyPressed() {
