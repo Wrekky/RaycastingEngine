ac33a34d84fa4ef85b146bfe92823a66029967f0
Andrew Friesen <andrewf5000@gmail.com>
2025-03-12 23:44:49 -0400

    Created SDL Window

--- raycasting-c/RaycastingProject/RaycastingProject/Main.c
+++ raycasting-c/RaycastingProject/RaycastingProject/Main.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 #include <SDL3/SDL.h>
+#include "constants.h"
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+int isGameRunning = FALSE;
+int initializedWindow() {
+	//SDL_Init(SDL_Init);
+	window = SDL_CreateWindow(NULL, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_BORDERLESS);
+
+	if (!window) {
+		fprintf(stderr, "Error creating SDL window. \n");
+		return FALSE;
+	}
+
+	renderer = SDL_CreateRenderer(window, "test");
+	if (!renderer) {
+		fprintf(stderr, "Error creating SDL renderer.\n");
+	}
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+	return TRUE;
+}
+
+void destroyWindow() {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
 int main(int argc, char* args[]) {
-	SDL_Init(SDL_Init);
-	printf("Hello world!");
+	isGameRunning = initializeWindow();
+	while (isGameRunning) {
+
+	}
+
+	destroyWindow();
+
 	return 0;
 }
