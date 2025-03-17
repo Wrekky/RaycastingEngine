f5a21cccce2ae644e6488cf62c8aa491c7b8bb11
Andrew Friesen <andrewf5000@gmail.com>
2025-03-17 18:40:08 -0400

    Fixed some issues with vertical intersection code.

--- raycasting-c/RaycastingProject/RaycastingProject/Main.c
+++ raycasting-c/RaycastingProject/RaycastingProject/Main.c
@@ -2,6 +2,8 @@
 #include <SDL3/SDL.h>
 #include "constants.h"
 #include <limits.h>
+
+
 const int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1},
 	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -79,8 +81,8 @@ void setup() {
 
 	player.x = WINDOW_WIDTH / 2;
 	player.y = WINDOW_HEIGHT / 2;
-	player.width = 5;
-	player.height = 5;
+	player.width = 1;
+	player.height = 1;
 	player.turnDirection = 0;
 	player.walkDirection = 0;
 	player.rotationAngle = PI / 2; //90 degrees
@@ -101,6 +103,7 @@ int hasWallAt(float x, float y) {
 
 void movePlayer(float deltaTime) {
 	player.rotationAngle += player.turnDirection * player.turnSpeed * deltaTime;
+	//player.rotationAngle = normalizeAngle(player.rotationAngle);
 	int moveStep = player.walkSpeed * player.walkDirection * deltaTime;
 	float newPlayerX = (player.x + (cos(player.rotationAngle) * moveStep));
 	float newPlayerY = (player.y + (sin(player.rotationAngle) * moveStep));
@@ -133,23 +136,24 @@ void renderPlayer() {
 		);
 }
 
-float normalizeAngle(rayAngle) {
-	float angle = remainder(rayAngle, (TWO_PI));
+float normalizeAngle(angle) {
+	angle = remainder(angle, TWO_PI);
 	if (angle < 0) {
-		angle = TWO_PI;
+		angle = TWO_PI + angle;
 	}
 	return angle;
 }
 
-float distanceBetweenPoints(x1, y1, x2, y2) {
-	return sqrt(((x2-x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+float distanceBetweenPoints(float x1, float y1, float x2, float y2) {
+	return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
 
 void castRay(float rayAngle, int stripId) {
-	rayAngle = normalizeAngle(rayAngle);
+	//rayAngle = normalizeAngle(rayAngle);
 
 	int isRayFacingDown = rayAngle > 0 && rayAngle < PI;
 	int isRayFacingUp = !isRayFacingDown;
+
 	int isRayFacingRight = rayAngle < 0.5 * PI || rayAngle > 1.5 * PI;
 	int isRayFacingLeft = !isRayFacingRight;
 	float xIntercept, yIntercept;
@@ -172,7 +176,7 @@ void castRay(float rayAngle, int stripId) {
 	xStep = TILE_SIZE / tan(rayAngle);
 	xStep *= (isRayFacingLeft && xStep > 0) ? -1 : 1;
 	xStep *= (isRayFacingRight && xStep < 0) ? -1 : 1;
-
+	////allsame
 	float nextHorzTouchX = xIntercept;
 	float nextHorzTouchY = yIntercept;
 
@@ -199,21 +203,20 @@ void castRay(float rayAngle, int stripId) {
 	float vertWallHitX = 0;
 	float vertWallHitY = 0;
 	int vertHitType = 0;
-	yIntercept = floor(player.x / TILE_SIZE) * TILE_SIZE;
-	yIntercept = isRayFacingRight ? TILE_SIZE : 0;
+	xIntercept = floor(player.x / TILE_SIZE) * TILE_SIZE;
+	xIntercept = isRayFacingRight ? TILE_SIZE : 0;
 
-	xIntercept = player.y + (xIntercept - player.x) / tan(rayAngle);
+	yIntercept = player.y + (xIntercept - player.x) * tan(rayAngle);
 
 	xStep = TILE_SIZE;
 	xStep *= isRayFacingLeft ? -1 : 1;
 
-	yStep = TILE_SIZE / tan(rayAngle);
-	yStep *= (isRayFacingUp && xStep > 0) ? -1 : 1;
-	yStep *= (isRayFacingDown && xStep < 0) ? -1 : 1;
+	yStep = TILE_SIZE * tan(rayAngle);
+	yStep *= (isRayFacingUp && yStep > 0) ? -1 : 1;
+	yStep *= (isRayFacingDown && yStep < 0) ? -1 : 1;
 
 	float nextVertTouchX = xIntercept;
 	float nextVertTouchY = yIntercept;
-
 	while (nextVertTouchX >= 0 && nextVertTouchX <= WINDOW_WIDTH && nextVertTouchY >= 0 && nextVertTouchY <= WINDOW_HEIGHT) {
 		float xToCheck = nextVertTouchX + (isRayFacingLeft ? -1 : 0);
 		float yToCheck = nextVertTouchY;
