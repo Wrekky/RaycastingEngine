c2701921040f1c3f228813e1b3db8d9eaec867a9
Andrew Friesen <andrewf5000@gmail.com>
2025-04-15 13:10:08 -0400

    Added sprite isVisible detection, only add sprites to projection render if they are within the field of view.

--- raycasting-c/RaycastingProject/RaycastingProject/sprite.c
+++ raycasting-c/RaycastingProject/RaycastingProject/sprite.c
@@ -1,9 +1,5 @@
 #include "sprite.h"
-#include "graphics.h"
-#include "textures.h"
-#include "player.h"
-#include "defs.h"
-#include "color.h"
+
 #define NUM_SPRITES 1
 
 static sprite_t sprites[NUM_SPRITES] = {
@@ -12,10 +8,31 @@ static sprite_t sprites[NUM_SPRITES] = {
 
 void renderMapSprites() {
 	for (int i = 0; i < NUM_SPRITES; i++) {
-		drawRect(2, 2, sprites[i].x * MINIMAP_SCALE_FACTOR, sprites[i].y * MINIMAP_SCALE_FACTOR, BLUE);
+		drawRect(2, 2, sprites[i].x * MINIMAP_SCALE_FACTOR, sprites[i].y * MINIMAP_SCALE_FACTOR, sprites[i].isVisible ? BLUE : GREEN);
 	}
 }
 
 void renderSpriteProjection() {
+	sprite_t visibleSprites[NUM_SPRITES];
+	int numVisibleSprites = 0;
 
+	for (int i = 0; i < NUM_SPRITES; i++) {
+		float angleSpritePlayer = player.rotationAngle - atan2(sprites[i].y - player.y, sprites[i].x - player.x);
+
+		if (angleSpritePlayer > PI) {
+			angleSpritePlayer -= TWO_PI;
+		}
+		if (angleSpritePlayer < -PI) {
+			angleSpritePlayer += TWO_PI;
+		}
+		angleSpritePlayer = fabs(angleSpritePlayer);
+		if (angleSpritePlayer < (FOV_ANGLE / 2)) {
+			sprites[i].isVisible = true;
+			visibleSprites[numVisibleSprites] = sprites[i];
+			numVisibleSprites++;
+		}
+		else {
+			sprites[i].isVisible = false;
+		}
+	}
 }
--- raycasting-c/RaycastingProject/RaycastingProject/sprite.h
+++ raycasting-c/RaycastingProject/RaycastingProject/sprite.h
@@ -1,12 +1,17 @@
 #ifndef SPRITE_H	
 #define SRITE_H
-
+#include "graphics.h"
+#include "textures.h"
+#include "player.h"
+#include "defs.h"
+#include "color.h"
 typedef struct {
 	float x;
 	float y;
 	float distance;
 	float angle;
 	int textureIndex;
+	bool isVisible;
 } sprite_t;
 
 void renderSpriteProjection();
