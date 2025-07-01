a4f56bf19220954dadd1fd297044697e4e1d81d4
Andrew Friesen <andrewf5000@gmail.com>
2025-06-30 20:13:29 -0400

    Added opacity for sprite drawing.

--- raycasting-c/RaycastingProject/RaycastingProject/sprite.c
+++ raycasting-c/RaycastingProject/RaycastingProject/sprite.c
@@ -70,7 +70,9 @@ void renderSpriteProjection() {
 				if (x > 0 && x < WINDOW_WIDTH && y > 0 && y < WINDOW_HEIGHT) {
 					color_t* textureBuffer = (color_t*)upng_get_buffer(textures[sprite.textureIndex]);
 					color_t texturePixelColor = textureBuffer[(textureWidth * textureOffsetY) + textureOffsetX];
-					drawPixel(x, y, texturePixelColor);
+					if (texturePixelColor != 0xFFFF00FF) {
+						drawPixel(x, y, texturePixelColor);
+					}
 				}
 			}
 		}
